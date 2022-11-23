181bbc1c49db0c5c8c1ae6d0fea376b67c3bcc4b
kunal <kunalgarg42@gmail.com>
2022-11-22 21:01:01 -0500

    Changed CF state and safe limits

--- train_and_test/Crazyflie_train_new.py
+++ train_and_test/Crazyflie_train_new.py
@@ -111,7 +111,7 @@ def main(args):
     dataset = Dataset_with_Grad(n_state=n_state, m_control=m_control, train_u=train_u)
     trainer = Trainer(cbf, dataset, n_state=n_state, m_control=m_control, j_const=2, dyn=dynamics,
                       n_pos=1,
-                      dt=dt, safe_alpha=0.3, dang_alpha=0.4, action_loss_weight=0.001, params=nominal_params,
+                      dt=dt, action_loss_weight=0.001, params=nominal_params,
                       fault=fault, gpu_id=0,
                       fault_control_index=fault_control_index)
     loss_np = 1.0
