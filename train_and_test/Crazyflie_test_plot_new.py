51fdaf22efbe2237c95c64756f771e379c4cc4cf
Charles Dawson <charles.dwsn@gmail.com>
2022-11-29 16:49:12 -0500

    Plotting changes

--- train_and_test/Crazyflie_test_plot_new.py
+++ train_and_test/Crazyflie_test_plot_new.py
@@ -7,9 +7,14 @@ import math
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import tqdm
 
-sys.path.insert(1, os.path.abspath('..'))
-sys.path.insert(1, os.path.abspath('.'))
+import seaborn as sns
+
+sns.set_theme(context="paper", font_scale=2.0, style="ticks")
+
+sys.path.insert(1, os.path.abspath(".."))
+sys.path.insert(1, os.path.abspath("."))
 
 # from dynamics.fixed_wing_dyn import fw_dyn_ext, fw_dyn
 from dynamics.Crazyflie import CrazyFlies
@@ -20,31 +25,9 @@ from trainer.trainer import Trainer
 from trainer.utils import Utils
 from trainer.NNfuncgrad_CF import CBF, alpha_param, NNController_new
 
-xg = torch.tensor([0.0,
-                   0.0,
-                   3.5,
-                   0.0,
-                   0.0,
-                   0.0,
-                   0.0,
-                   0.0,
-                   0.0,
-                   0.0,
-                   0.0,
-                   0.0])
+xg = torch.tensor([0.0, 0.0, 3.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
-x0 = torch.tensor([[2.0,
-                    2.0,
-                    5.1,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0,
-                    0.0]])
+x0 = torch.tensor([[2.0, 2.0, 5.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
 dt = 0.001
 n_state = 12
 m_control = 4
@@ -60,8 +43,15 @@ fault_known = 1
 
 def main():
     dynamics = CrazyFlies(x=x0, goal=xg, nominal_params=nominal_params, dt=dt)
-    util = Utils(n_state=n_state, m_control=m_control, dyn=dynamics, params=nominal_params, fault=fault,
-                 fault_control_index=fault_control_index, j_const=2)
+    util = Utils(
+        n_state=n_state,
+        m_control=m_control,
+        dyn=dynamics,
+        params=nominal_params,
+        fault=fault,
+        fault_control_index=fault_control_index,
+        j_const=2,
+    )
 
     # NN_controller = NNController_new(n_state=n_state, m_control=m_control)
     NN_cbf = CBF(dynamics, n_state=n_state, m_control=m_control)
@@ -69,13 +59,21 @@ def main():
     FT_cbf = CBF(dynamics, n_state=n_state, m_control=m_control)
     try:
         # NN_controller.load_state_dict(torch.load('./good_data/data/CF_controller_NN_weightsCBF.pth'))
-        NN_cbf.load_state_dict(torch.load('./good_data/data/CF_cbf_NN_weightsCBF.pth', map_location='cpu'))
-        FT_cbf.load_state_dict(torch.load('./good_data/data/CF_cbf_FT_weightsCBF.pth', map_location='cpu'))
+        NN_cbf.load_state_dict(
+            torch.load("./good_data/data/CF_cbf_NN_weightsCBF.pth", map_location="cpu")
+        )
+        FT_cbf.load_state_dict(
+            torch.load("./good_data/data/CF_cbf_FT_weightsCBF.pth", map_location="cpu")
+        )
         # NN_alpha.load_state_dict(torch.load('./good_data/data/CF_alpha_NN_weights.pth'))
     except:
         # NN_controller.load_state_dict(torch.load('./data/CF_controller_NN_weights.pth'))
-        NN_cbf.load_state_dict(torch.load('./data/CF_cbf_NN_weightsCBF.pth', map_location='cpu'))
-        FT_cbf.load_state_dict(torch.load('./data/CF_cbf_FT_weightsCBF.pth', map_location='cpu'))
+        NN_cbf.load_state_dict(
+            torch.load("./data/CF_cbf_NN_weightsCBF.pth", map_location="cpu")
+        )
+        FT_cbf.load_state_dict(
+            torch.load("./data/CF_cbf_FT_weightsCBF.pth", map_location="cpu")
+        )
         # NN_alpha.load_state_dict(torch.load('./data/CF_alpha_NN_weights.pth'))
 
     NN_cbf.eval()
@@ -114,6 +112,7 @@ def main():
 
     x_pl = np.array(state).reshape(1, n_state)
     fault_activity = np.array([0])
+    detected_fault_activity = np.zeros((1 + config.EVAL_STEPS,))
     u_pl = np.array([0] * m_control).reshape(1, m_control)
     h, _ = NN_cbf.V_with_jacobian(state.reshape(1, n_state, 1))
 
@@ -134,7 +133,7 @@ def main():
     # u_samples = np.hstack((u_samples, alpha_samples))
     # u_samples = torch.tensor(u_samples, dtype=torch.float32)
 
-    for i in range(config.EVAL_STEPS):
+    for i in tqdm.trange(config.EVAL_STEPS):
         # print(i)
         # if state[0, 2] > goal[0, 2]:
         #     u_nominal = u_eq.clone() * (1 + torch.linalg.norm(state[0, 2]-goal[0, 2]) / 1000)
@@ -155,8 +154,11 @@ def main():
             # 1 -> time-based switching, assumes knowledge of when fault occurs and stops
             # 0 -> Fault-detection based-switching, using the proposed scheme from the paper
 
-            if fault_start == 0 and fault_start_epoch <= i <= fault_start_epoch + fault_duration / 5 and util.is_safe(
-                    state):
+            if (
+                fault_start == 0
+                and fault_start_epoch <= i <= fault_start_epoch + fault_duration / 5
+                and util.is_safe(state)
+            ):
                 fault_start = 1
 
             if fault_start == 1 and i > fault_start_epoch + fault_duration:
@@ -193,6 +195,7 @@ def main():
             u = u.reshape(1, m_control)
 
             if fault_start == 1:
+                detected_fault_activity[i] = 1.0
                 u[0, fault_control_index] = torch.rand(1) / 4
 
             for j in range(m_control):
@@ -208,17 +211,19 @@ def main():
 
         else:
             h, grad_h = NN_cbf.V_with_jacobian(state.reshape(1, n_state, 1))
-            u = NN_controller(state, u_nominal)
-            u = util.neural_controller(u, fx, gx, h, grad_h, fault_start)
+            u = util.neural_controller(u_nominal, fx, gx, h, grad_h, fault_start)
 
             if fault_start_epoch <= i <= fault_start_epoch + fault_duration:
-                u[fault_control_index] = torch.rand(1) / 4
+                fault_start = 1.0
+                u[0, fault_control_index] = torch.rand(1) / 4
+            else:
+                fault_start = 0.0
 
             for j in range(m_control):
-                if u[0, j] < ul[j]:
-                    u[j] = ul[j].clone()
-                if u[0, j] > um[j]:
-                    u[j] = um[j].clone()
+                if u[0, j] < ul[0, j]:
+                    u[0, j] = ul[0, j].clone() * 2
+                if u[0, j] > um[0, j]:
+                    u[0, j] = um[0, j].clone() / 2
 
             u = torch.tensor(u, dtype=torch.float32)
             gxu = torch.matmul(gx, u.reshape(m_control, 1))
@@ -226,24 +231,26 @@ def main():
             dx = fx.reshape(1, n_state) + gxu.reshape(1, n_state)
 
             dot_h = torch.matmul(dx, grad_h.reshape(n_state, 1))
+
             if dot_h < epsilon:
                 h, grad_h = FT_cbf.V_with_jacobian(state.reshape(1, n_state, 1))
-                u = FT_controller(state, u_nominal)
-                u = util.neural_controller(u, fx, gx, h, grad_h, fault_start)
+                u = util.neural_controller(u_nominal, fx, gx, h, grad_h, fault_start)
                 u = torch.squeeze(u.detach().cpu())
                 if fault_start_epoch <= i <= fault_start_epoch + fault_duration:
                     u[fault_control_index] = torch.rand(1) / 4
 
                 for j in range(m_control):
-                    if u[0, j] <= ul[j]:
-                        u[j] = ul[j].clone()
-                    if u[0, j] >= um[j]:
-                        u[j] = um[j].clone()
+                    if u[j] < ul[0, j]:
+                        u[j] = ul[0, j].clone() * 2
+                    if u[j] > um[0, j]:
+                        u[j] = um[0, j].clone() / 2
 
                 u = torch.tensor(u, dtype=torch.float32)
                 gxu = torch.matmul(gx, u.reshape(m_control, 1))
 
                 dx = fx.reshape(1, n_state) + gxu.reshape(1, n_state)
+
+                detected_fault_activity[i] = 1.0
 
         # dot_h = torch.matmul(dx, grad_h.reshape(n_state, 1))
         # print(u)
@@ -255,9 +262,14 @@ def main():
 
         is_safe = int(util.is_safe(state))
         is_unsafe = int(util.is_unsafe(state))
-        safety_rate = safety_rate * (1 - 1 / config.EVAL_STEPS) + is_safe / config.EVAL_STEPS
+        safety_rate = (
+            safety_rate * (1 - 1 / config.EVAL_STEPS) + is_safe / config.EVAL_STEPS
+        )
         unsafety_rate += is_unsafe / config.EVAL_STEPS
-        h_correct += is_safe * int(h >= 0) / config.EVAL_STEPS + is_unsafe * int(h < 0) / config.EVAL_STEPS
+        h_correct += (
+            is_safe * int(h >= 0) / config.EVAL_STEPS
+            + is_unsafe * int(h < 0) / config.EVAL_STEPS
+        )
         dot_h_correct += torch.sign(dot_h.clone().detach()) / config.EVAL_STEPS
 
         x_pl = np.vstack((x_pl, np.array(state.clone().detach()).reshape(1, n_state)))
@@ -267,7 +279,7 @@ def main():
 
         state = state_next.clone()
         # print('h, {}, dot_h, {}'.format(h.detach().cpu().numpy()[0][0], dot_h.detach().cpu().numpy()[0][0]))
-    time_pl = np.arange(0., dt * config.EVAL_STEPS + dt, dt)
+    time_pl = np.arange(0.0, dt * config.EVAL_STEPS + dt, dt)
 
     z_pl = x_pl[:, 2]
 
@@ -285,39 +297,131 @@ def main():
     u3 = u_pl[:, 2]
     u4 = u_pl[:, 3]
 
-    fig = plt.figure(figsize=(18, 10))
-    fig.tight_layout(pad=5.0)
+    colors = sns.color_palette()
 
-    ax1 = plt.subplot(331)
-    ax1.plot(time_pl, z_pl)
-    ax1.title.set_text('z')
-    ax2 = plt.subplot(332)
-    ax2.plot(time_pl, u2, '--g')
-    ax2.title.set_text('u2')
-    ax3 = plt.subplot(333)
-    ax3.plot(time_pl, u1, '--r')
-    ax3.title.set_text('u1')
-    ax4 = plt.subplot(334)
-    ax4.plot(time_pl, h_pl)
-    ax4.title.set_text('CBF value')
-    ax5 = plt.subplot(335)
-    ax5.plot(time_pl, u3, '--b')
-    ax5.title.set_text('u3')
-    ax6 = plt.subplot(336)
-    ax6.plot(time_pl, u4, '--y')
-    ax6.title.set_text('u4')
-    ax7 = plt.subplot(337)
-    ax7.plot(time_pl, fault_activity, '--g')
-    ax7.title.set_text('fault_activity')
-    ax8 = plt.subplot(338)
-    ax8.plot(time_pl, p_pl, '--g')
-    ax8.title.set_text('Angle theta')
-    ax9 = plt.subplot(339)
-    ax9.plot(time_pl, ps_pl, '--r')
-    ax9.title.set_text('Angle phi')
+    fig = plt.figure(figsize=(12, 14))
+    fig.tight_layout(pad=1.15)
+    axs = fig.subplots(2, 1)
+
+    # Plot the altitude and CBF value on one axis
+    z_ax = axs[0]
+    z_ax.plot(time_pl, z_pl, linewidth=4.0, label="z (m)", color=colors[0])
+    z_ax.plot(
+        time_pl,
+        0 * time_pl,
+        color="k",
+        linestyle="--",
+        linewidth=4.0,
+        label="Unsafe boundary",
+    )
+    z_ax.set_ylabel("Height (m)", color=colors[0])
+    z_ax.set_xlabel("Time (s)")
+    z_ax.set_xlim(time_pl[0], time_pl[-1])
+    z_ax.tick_params(axis="y", labelcolor=colors[0])
+
+    h_ax = z_ax.twinx()
+    h_ax.plot(time_pl, h_pl, linestyle="-", linewidth=4.0, color=colors[1])
+    h_ax.plot(
+        time_pl,
+        0 * time_pl,
+        color="k",
+        linestyle="--",
+        linewidth=4.0,
+        label="Unsafe boundary",
+    )
+    h_ax.set_ylabel("CBF value", color=colors[1])
+    h_ax.tick_params(axis="y", labelcolor=colors[1])
+
+    # Plot the control action on another axis
+    u_ax = axs[1]
+    u_ax.plot(time_pl, u2, linewidth=2.0, label="$u_2$ (faulty)")
+    u_ax.plot(time_pl, u1, linewidth=2.0, label="$u_1$")
+    u_ax.plot(time_pl, u3, linewidth=2.0, label="$u_3$")
+    u_ax.plot(time_pl, u4, linewidth=2.0, label="$u_4$")
+    u_ax.set_xlabel("Time (s)")
+    u_ax.set_ylabel("Control effort")
+    u_ax.set_xlim(time_pl[0], time_pl[-1])
+    u_ax.legend()
+
+    # Add the fault indicators
+    (t_fault_start, t_fault_end) = time_pl[
+        np.diff(fault_activity.squeeze()).nonzero()[0]
+    ]
+    lims = z_ax.get_ylim()
+    z_ax.fill_between(
+        [t_fault_start, t_fault_end],
+        [-10.0, -10.0],
+        [10.0, 10.0],
+        color="grey",
+        alpha=0.5,
+        label="Fault",
+    )
+    z_ax.set_ylim(lims)
+    detected_mask = detected_fault_activity.nonzero()
+    z_ax.plot(
+        time_pl[detected_mask],
+        detected_fault_activity[detected_mask],
+        color=colors[3],
+        label="Fault detected",
+        linewidth=4.0,
+    )
+    z_ax.plot(
+        [time_pl[detected_mask].min(), time_pl[detected_mask].max()],
+        [1.0, 1.0],
+        "o",
+        color=colors[3],
+        markersize=15.0
+    )
+    z_ax.legend()
+    lims = u_ax.get_ylim()
+    u_ax.fill_between(
+        [t_fault_start, t_fault_end],
+        [-1.0, -1.0],
+        [1.0, 1.0],
+        color="grey",
+        alpha=0.5,
+        label="Fault",
+    )
+    u_ax.set_ylim(lims)
+    # h_ax.plot([t_fault_start, t_fault_start], lims, "k:", linewidth="5.0")
+    # h_ax.plot([t_fault_end, t_fault_end], lims, "k:", linewidth="5.0")
+
+    # plt.legend()
+
+    # fig = plt.figure(figsize=(18, 10))
+    # fig.tight_layout(pad=5.0)
+
+    # ax1 = plt.subplot(331)
+    # ax1.plot(time_pl, z_pl)
+    # ax1.title.set_text('z')
+    # ax2 = plt.subplot(332)
+    # ax2.plot(time_pl, u2, '--g')
+    # ax2.title.set_text('u2')
+    # ax3 = plt.subplot(333)
+    # ax3.plot(time_pl, u1, '--r')
+    # ax3.title.set_text('u1')
+    # ax4 = plt.subplot(334)
+    # ax4.plot(time_pl, h_pl)
+    # ax4.title.set_text('CBF value')
+    # ax5 = plt.subplot(335)
+    # ax5.plot(time_pl, u3, '--b')
+    # ax5.title.set_text('u3')
+    # ax6 = plt.subplot(336)
+    # ax6.plot(time_pl, u4, '--y')
+    # ax6.title.set_text('u4')
+    # ax7 = plt.subplot(337)
+    # ax7.plot(time_pl, fault_activity, '--g')
+    # ax7.title.set_text('fault_activity')
+    # ax8 = plt.subplot(338)
+    # ax8.plot(time_pl, p_pl, '--g')
+    # ax8.title.set_text('Angle theta')
+    # ax9 = plt.subplot(339)
+    # ax9.plot(time_pl, ps_pl, '--r')
+    # ax9.title.set_text('Angle phi')
     # plt.suptitle('Categorical Plotting')
-    plt.savefig('./plots/plot_closed_loop_data_CF.png')
+
+    plt.savefig("./plots/plot_closed_loop_data_CF.png")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
