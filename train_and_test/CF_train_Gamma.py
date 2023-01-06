beb1e7e1a96f2fc4dd68ffdbb17bd9fa7a8dec36
Kunal Garg <kunalgarg42@gmail.com>
2023-01-06 17:32:00 -0500

    big_batch_gamma

--- train_and_test/CF_train_Gamma.py
+++ train_and_test/CF_train_Gamma.py
@@ -53,9 +53,9 @@ fault = nominal_params["fault"]
 
 init_param = 1  # int(input("use previous weights? (0 -> no, 1 -> yes): "))
 
-n_sample = 1000
+n_sample = 500
 
-n_sample_data = 1000
+n_sample_data = 500
 
 traj_len = 100
 
@@ -95,7 +95,7 @@ def main(args):
     cbf.load_state_dict(torch.load('./good_data/data/CF_cbf_NN_weightsCBF.pth'))
     cbf.eval()
 
-    dataset = Dataset_with_Grad(n_state=n_state, m_control=m_control, train_u=0, buffer_size=n_sample_data*traj_len*10, traj_len=traj_len)
+    dataset = Dataset_with_Grad(n_state=n_state, m_control=m_control, train_u=0, buffer_size=n_sample_data*traj_len*20, traj_len=traj_len)
     trainer = Trainer(cbf, dataset, gamma=gamma, n_state=n_state, m_control=m_control, j_const=2, dyn=dynamics,
                       dt=dt, action_loss_weight=0.001, params=nominal_params,
                       fault=fault, gpu_id=gpu_id, num_traj=n_sample, traj_len=traj_len,
