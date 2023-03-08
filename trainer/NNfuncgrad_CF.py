5c8da66942fd77fd0e62420f396dcde22fd38615
Kunal Garg <kunalgarg42@gmail.com>
2023-03-08 10:58:53 -0500

    new linear gamma NN

--- trainer/NNfuncgrad_CF.py
+++ trainer/NNfuncgrad_CF.py
@@ -329,4 +329,51 @@ class Gamma(nn.Module):
         x = self.output_activation(self.fc2(x))
         gamma = x ** 2
         return gamma
+
+
+class Gamma_linear(nn.Module):
+
+    def __init__(self, n_state, m_control, traj_len, preprocess_func=None):
+        super().__init__()
+        self.n_state = n_state
+        # self.k_obstacle = k_obstacle
+        self.m_control = m_control
+        self.preprocess_func = preprocess_func
+
+        self.conv0 = nn.Linear((2 * n_state + m_control) * traj_len, 64, 1)
+        self.conv1 = nn.Linear(64, 128, 1)
+        self.conv2 = nn.Linear(128, 128, 1)
+        self.fc0 = nn.Linear(128, 128)
+        self.fc1 = nn.Linear(128, 64)
+        self.fc2 = nn.Linear(64, m_control)
+        self.activation = nn.ReLU()
+        self.output_activation = nn.Tanh()
+
+    def forward(self, state, state_diff, u):
+        """
+        args:
+            state (bs, traj_len, n_state)
+            u (bs, traj_len, m_control)
+        returns:
+            gamma (bs, m_control)
+        """
+
+        state = torch.cat([state, state_diff, u], dim=-1)
+        state_shape = state.shape
+
+        state = state.reshape(state_shape[0], state_shape[1] * state_shape[2])
+
+        if self.preprocess_func is not None:
+            state = self.preprocess_func(state)
+            # state_error = self.preprocess_func(state_error)
         
+        x = self.activation(self.conv0(state))
+        x = self.activation(self.conv1(x))
+        x = self.activation(self.conv2(x))  # (bs, 128, k_obstacle)
+        # x, _ = torch.max(x, dim=2)  # (bs, 128)
+        # x = torch.cat([x, u_nominal], dim=1)  # (bs, 128 + m_control)
+        x = self.activation(self.fc0(x))
+        x = self.activation(self.fc1(x))
+        x = self.output_activation(self.fc2(x))
+
+        return x
